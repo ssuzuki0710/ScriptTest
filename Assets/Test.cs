6cdba1053491d50d522724940d584985fe402afc
鈴木成惇 <p088_sesemida@yahoo.co.jp>
2017-03-26 23:20:18 +0900

    ScriptTestプロジェクト　初めてのコミット
    Lesson4の基本課題

---
+++ Assets/Test.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // 要素数5の配列を初期化する
+        int[] points = new int[5];
+
+        // 配列の各要素に値を代入する
+        points[0] = 77;
+        points[1] = 18;
+        points[2] = 50;
+        points[3] = 68;
+        points[4] = 99;
+
+        // 配列の要素をすべて表示する
+        for (int array = 4; array >= 0; array--)
+        {
+            Debug.Log(points[array]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
